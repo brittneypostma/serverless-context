7fa338dee18afdbd93d2ec43f00d25d3c191ae4e
brittneypostma <bdesignedwebdev@gmail.com>
2022-11-04 10:45:58 -0400

    console context

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event: RequestEvent) => {
 	const context = event.platform?.context;
-
+	console.log(context);
 	return {
 		context
 	};
